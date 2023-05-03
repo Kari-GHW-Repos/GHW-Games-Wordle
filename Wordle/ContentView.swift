75d1287c055b15bdbda162f8387143cc4e163a02
KarolinaGroszewska <karolina.groszewska@gmail.com>
2023-05-03 17:26:05 -0500

    i broke something

--- Wordle/ContentView.swift
+++ Wordle/ContentView.swift
@@ -7,8 +7,17 @@
 
 import SwiftUI
 
+var words = ["About","Alert","Argue","Beach", "Above","Alike","Arise","Began", "Abuse","Alive","Array","Begin","Actor","Allow","Aside","Begun", "Acute","Alone","Asset","Being", "Admit","Along","Audio","Below", "Adopt","Alter","Audit","Bench", "Adult","Among","Avoid","Billy", "After","Anger","Award","Birth", "Again","Angle","Aware","Black", "Agent","Angry","Badly","Blame", "Agree","Apart","Baker","Blind", "Ahead","Apple","Bases","Block", "Alarm","Apply","Basic","Blood", "Album","Arena","Basis","Board", "Boost","Buyer","China","Cover", "Booth","Cable","Chose","Craft", "Bound","Calif","Civil","Crash", "Brain","Carry","Claim","Cream", "Brand","Catch","Class","Crime","Bread","Cause","Clean","Cross","Break","Chain","Clear","Crowd", "Breed","Chair","Click","Crown", "Brief","Chart","Clock","Curve", "Bring","Chase","Close","Cycle", "Broad","Cheap","Coach","Daily", "Broke","Check","Coast","Dance", "Brown","Chest","Could","Dated","Build","Chief","Count","Dealt","Built","Child","Court","Death","Debut","Entry","Forth","Group","Delay","Equal","Forty","Grown","Depth","Error","Forum","Guard","Doing","Event","Found","Guess","Doubt","Every","Frame","Guest","Dozen","Exact","Frank","Guide", "Draft","Exist","Fraud","Happy", "Drama","Extra","Fresh","Harry", "Drawn","Faith","Front","Heart","Dream","False","Fruit","Heavy", "Dress","Fault","Fully","Hence","Drill","Fiber","Funny","Night","Drink","Field","Giant","Horse", "Drive","Fifth","Given","Hotel", "Drove","Fifty","Glass","House", "Dying","Fight","Globe","Human","Eager","Final","Going","Ideal", "Early","First","Grace","Image", "Earth","Fixed","Grade","Index", "Eight","Flash","Grand","Inner", "Elite","Fleet","Grant","Input","Empty","Floor","Grass","Issue", "Enemy","Fluid","Great","Irony", "Enjoy","Focus","Green","Juice","Enter","Force","Gross","Joint", "Judge","Metal","Media","Newly", "Known","Local","Might","Noise", "Label","Logic","Minor","North", "Large","Loose","Minus","Noted", "Laser","Lower","Mixed","Novel", "Later","Lucky","Model","Nurse", "Laugh","Lunch","Money","Occur", "Layer","Lying","Month","Ocean", "Learn","Magic","Moral","Offer", "Lease","Major","Motor","Often", "Least","Maker","Mount","Order", "Leave","March","Mouse","Other", "Legal","Music","Mouth","Ought", "Level","Match","Movie","Paint", "Light","Mayor","Needs","Paper", "Limit","Meant","Never","Party", "Peace","Power","Radio","Round", "Panel","Press","Raise","Route","Phase","Price","Range","Royal","Phone","Pride","Rapid","Rural", "Photo","Prime","Ratio","Scale", "Piece","Print","Reach","Scene", "Pilot","Prior","Ready","Scope", "Pitch","Prize","Refer","Score","Place","Proof","Right","Sense", "Plain","Proud","Rival","Serve", "Plane","Prove","River","Seven","Plant","Queen","Quick","Shall", "Plate","Sixth","Stand","Shape", "Point","Quiet","Roman","Share", "Pound","Quite","Rough","Sharp", "Sheet","Spare","Style","Times", "Shelf","Speak","Sugar","Tired", "Shell","Speed","Suite","Title","Shift","Spend","Super","Today", "Shirt","Spent","Sweet","Topic","Shock","Split","Table","Total", "Shoot","Spoke","Taken","Touch", "Short","Sport","Taste","Tough", "Shown","Staff","Taxes","Tower","Sight","Stage","Teach","Track", "Since","Stake","Teeth","Trade", "Sixty","Start","Texas","Treat", "Sized","State","Thank","Trend","Steak", "Skill","Steam","Theft","Trial", "Sleep","Steel","Their","Tried", "Slide","Stick","Theme","Tries", "Small","Still","There","Truck", "Smart","Stock","These","Truly", "Smile","Stone","Thick","Trust", "Smith","Stood","Thing","Truth","Smoke","Store","Think","Twice", "Solid","Storm","Third","Under","Solve","Story","Those","Undue", "Sorry","Strip","Three","Union", "Sound","Stuck","Threw","Unity", "South","Study","Throw","Until","Space","Stuff","Tight","Upper", "Upset","Whole","Waste","Wound", "Urban","Whose","Watch","Write", "Usage","Woman","Water","Wrong", "Usual","Train","Wheel","Wrote","Valid","World","Where","Yield", "Value","Worry","Which","Young", "Video","Worse","While","Youth","Virus","Worst","White","Worth","Visit","Would","Vital","Voice"]
+
+var randomWord = words.randomElement()!.uppercased()
+var randomLetter1 = String(randomWord[randomWord.startIndex])
+var randomLetter2 = String(randomWord[randomWord.index(randomWord.startIndex, offsetBy: 1)])
+var randomLetter3 = String(randomWord[randomWord.index(randomWord.startIndex, offsetBy: 2)])
+var randomLetter4 = String(randomWord[randomWord.index(randomWord.startIndex, offsetBy: 3)])
+var randomLetter5 = String(randomWord[randomWord.index(randomWord.startIndex, offsetBy: 4)])
+
+
 struct ContentView: View {
-    //TODO: Add list of valid words
     //TODO: Add auto-tabbing between words
     //TODO: Change word at 12am GMT every day
     //TODO: Make stats page for Apple Watch <3 
@@ -60,22 +69,22 @@ struct ContentView: View {
     @State var letter30 = ""
     @State var word6Submitted = false
 
-    @State var correctLetter1 = "T"
-    @State var correctLetter2 = "E"
-    @State var correctLetter3 = "S"
-    @State var correctLetter4 = "T"
-    @State var correctLetter5 = "S"
+    @State var correctLetter1 = randomLetter1
+    @State var correctLetter2 = randomLetter2
+    @State var correctLetter3 = randomLetter3
+    @State var correctLetter4 = randomLetter4
+    @State var correctLetter5 = randomLetter5
     
     @State var gameWon = false
-    //TODO: Change so that the color of the letter only shows upon submission
     //TODO: Move from one letter in the row to the next once finished typing
     @FocusState var focusedField: Int?
+
     var body: some View {
         ZStack{
             Color(red: 34/255, green: 34/255, blue: 34/255).ignoresSafeArea()
             VStack{
-                Text("GHW Games: Wordle!")
-                    .font(.title)
+                Text("GHW Games: Infinite Wordle!")
+                    .font(.title2)
                     .fontWeight(.bold)
                     .foregroundColor(.white)
                     .padding(.bottom, 60)
@@ -112,6 +121,8 @@ struct ContentView: View {
 }
 
 struct Word : View{
+    @State var attempts: Int = 0
+
     @Binding var letter1 : String
     @Binding var letter2 : String
     @Binding var letter3 : String
@@ -231,8 +242,22 @@ struct Word : View{
             }
             if !letter1.isEmpty && !letter2.isEmpty && !letter3.isEmpty && !letter4.isEmpty && !letter5.isEmpty{
                 Button("Submit word!") {
-                    word1Submitted = true
+                    print("letter1:", correctLetter1)
+                    print("letter2:",correctLetter2)
+                    print("letter3:",correctLetter3)
+                    print("letter4:",correctLetter4)
+                    print("letter5:",correctLetter5)
+
+                    if determineValidity(word: letter1 + letter2 + letter3 + letter4 + letter5) {
+                        word1Submitted = true
+                    }
+                    else{
+                        withAnimation(.default) {
+                            self.attempts += 1
+                        }
+                    }
                 }
+                .modifier(Shake(animatableData: CGFloat(attempts)))
                 .padding(.all, 5)
                 .background(Color(red: 1/255, green: 154/255, blue: 1/255))
                 .foregroundColor(.white)
@@ -253,9 +278,25 @@ struct Word : View{
             }
         }
     }
+    func determineValidity(word: String) -> Bool{
+        let newWord = word.capitalized(with: Locale.current)
+        return words.contains(newWord)
+    }
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Shake: GeometryEffect {
+    var amount: CGFloat = 10
+    var shakesPerUnit = 3
+    var animatableData: CGFloat
+
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        ProjectionTransform(CGAffineTransform(translationX:
+            amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)),
+            y: 0))
     }
 }
